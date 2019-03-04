9ba67ac7d3bf22b393f79293ee87c4fd98d3fd0e
kid07 <kohei16yamamoto2@gmail.com>
2019-03-05 00:22:56 +0900

    2019/2/05

--- src/master/src/test_export.cpp
+++ src/master/src/test_export.cpp
@@ -1,31 +1,39 @@
 // ros/ros.h　ROSに関する基本的なAPIのためのヘッダ
 #include "ros/ros.h"
-// comp_tutrial/adder.h　adder.msgから生成されたメッセージを定義しているヘッダ
+
+// geometry_msgs/Twiat.h　ヘッダファイル
 #include <geometry_msgs/Twist.h>
+
+void chatterCallback(const geometry_msgs::PoseWithCovariance pose)
+{
+    printf("x:%f  y:%f  z:%f\n",pose.x , pose.y, pose.z );
+    printf("x:%f  y:%f  z:%f  w:%f\n",pose.x , pose.y, pose.z, pose.w );
+}
 
 int main(int argc, char **argv)
 {
   // 初期化のためのAPI
-  // このノードは"para_in"という名前であるという意味
+  // このノードは"master"という名前であるという意味
   ros::init(argc, argv, "master");
 
   // ノードハンドラの宣言
   ros::NodeHandle nh;
 
   //Publisherとしての定義
-  // n.advertise<comp_tutorial::adder>("para_input", 1000);
-  // comp_tutorial::adder型のメッセージをpara_inputというトピックへ配信する
+  // n.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
+  // geometry_msgs::Twist型のメッセージを/cmd_velというトピックへ配信する
   //"1000"はトピックキューの最大値
   // ros::Publisher twist_pub = nh.advertise<geometry_msgs::Twist>("Twist/data", 1000);
 
   ros::Publisher twist_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
 
   //1秒間に1つのメッセージをPublishする
-  ros::Rate loop_rate(1);
+  ros::Rate loop_rate(10);
 
-  //comp_tutrial::adder型のオブジェクトを定義
-  //adder.msgで定義したa,bはメンバ変数としてアクセスできる
+  //geometry_msgs::Twist型のオブジェクトを定義
   geometry_msgs::Twist twist;
+
+  //初期値の定義
   twist.linear.x = 0.0;
   twist.linear.y = 0.0;
   twist.linear.z = 0.0;
@@ -33,11 +41,16 @@ int main(int argc, char **argv)
   twist.angular.y = 0.0;
   twist.angular.z = 1.0;
 
+  //ここからSubscriberの定義
+
+
+
+
   int count = 0;
   while (ros::ok())//ノードが実行中は基本的にros::ok()=1
   {
-//    twist.linear.x = count;
-  //  twist.angular.z = count;
+  //twist.linear.x = count;
+  //twist.angular.z = count;
     twist_pub.publish(twist);//PublishのAPI
     printf("a = %f b = %f \n",twist.linear.x  , twist.angular.z );
     ros::spinOnce();
