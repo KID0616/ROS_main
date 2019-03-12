e964776127ebeefe3d0e1abf3706287b6696bf80
kid07 <kohei16yamamoto2@gmail.com>
2019-03-12 14:08:42 +0900

    a

--- src/master/src/test_export.cpp
+++ src/master/src/test_export.cpp
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
   ros::Publisher twist_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
 
   //1秒間に1つのメッセージをPublishする
-  ros::Rate loop_rate(20);
+  ros::Rate loop_rate(50);
 
   //geometry_msgs::Twist型のオブジェクトを定義
 
