2733f3eb39c220a6f357f3c69d1754ebbc20a7b1
kid07 <kohei16yamamoto2@gmail.com>
2019-03-20 22:14:52 +0900

    a

--- src/master/src/servo_test.cpp
+++ src/master/src/servo_test.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     while (ros::ok())//ノードが実行中は基本的にros::ok()=1
     {
 
-        if (num == 10000) {
+        if (num == 1000) {
             num==0;
         }
         printf("%d\n",num);
