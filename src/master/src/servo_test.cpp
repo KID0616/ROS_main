fe5d07f7be40e3f355cef27d60bdeeeb055f4d1c
kid07 <kohei16yamamoto2@gmail.com>
2019-03-20 21:41:07 +0900

    a

--- src/master/src/servo_test.cpp
+++ src/master/src/servo_test.cpp
@@ -35,11 +35,11 @@ int main(int argc, char **argv)
         printf("%d\n",num);
         num++;
         set_PWM_dutycycle(pi,servo_num,num);
-        gpio_write(pi,servo_num,0);
+        //gpio_write(pi,servo_num,0);
         sleep(0.1);
         gpio_write(pi,servo_num,1);
         sleep(0.1);
-        gpio_write(pi,servo_num,0);
+        //gpio_write(pi,servo_num,0);
     }
   printf("end");
   pigpio_stop(pi);
