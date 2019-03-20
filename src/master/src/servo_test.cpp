5af2edfd2b18cf0a22c57eb83be26a6f1244297e
kid07 <kohei16yamamoto2@gmail.com>
2019-03-20 20:27:13 +0900

    a

--- src/master/src/servo_test.cpp
+++ src/master/src/servo_test.cpp
@@ -36,9 +36,9 @@ int main(int argc, char **argv)
         num++;
         set_PWM_dutycycle(pi,servo_num,num);
         gpio_write(pi,servo_num,0);
-        sleep(3);
+        sleep(0.1)
         gpio_write(pi,servo_num,1);
-        sleep(3);
+        sleep(0.1);
         gpio_write(pi,servo_num,0);
     }
   pigpio_stop(pi);
