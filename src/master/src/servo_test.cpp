abfa216795cf8614056f218e435b39cc2c92d252
kid07 <kohei16yamamoto2@gmail.com>
2019-03-26 14:19:53 +0900

    a

--- src/master/src/servo_test.cpp
+++ src/master/src/servo_test.cpp
@@ -15,10 +15,10 @@ int pi;
 void chatterCallback(const master::deg msg)
 {
     deg = msg.deg ;
-    num = 100 * deg / 180 + 25 ;
+    num = (100 * deg) / 180 + 25 ;
     //num=75 90deg 25:0deg 125:180deg
     set_PWM_dutycycle(pi,servo_num,num);
-    printf("deg:%d \n",num);
+    printf("deg:%d \n",deg);
     //sleep(1);
     gpio_write(pi,servo_num,1);
 }
